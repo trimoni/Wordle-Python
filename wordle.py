0013631d3b470dac3303c008016188ad15642813
Tal <trimoni8@gmail.com>
2022-12-05 11:28:52 -0500

    if statement

--- wordle.py
+++ wordle.py
@@ -11,5 +11,10 @@ print(guess)
 
 #Check First Letter
 
-
+if guess[0] == correct[0]:
+  print('CORRECT')
+elif guess[0] in correct:
+  print('IN WORD and WRONG SPOT')
+else:
+  print('NOT IN WORD')
 
